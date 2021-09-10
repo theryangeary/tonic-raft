3a25b8523162176e2b234d718591104ba54d08c9
Ryan Geary <rtgnj42@gmail.com>
2021-09-10 16:47:25 -0400

    Remove examples/build.rs

--- examples/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    tonic_build::configure()
-        .compile(&["proto/simple/simple.proto"], &["proto/simple"])
-        .unwrap_or_else(|e| panic!("Failed to compile protos: {:?}", e));
-}
